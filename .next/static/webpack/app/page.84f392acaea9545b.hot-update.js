9222ae3e1be64712f8aaa91b549c0a6ac3112fad
Abderrahmane Makhlouf <makhlouf.abderrahmane.31777315@gmail.com>
2025-11-05 07:50:32 +0100

    Updates

---
+++ .next/static/webpack/app/page.84f392acaea9545b.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./public/portfolioLogo.png":
+/*!**********************************!*\
+  !*** ./public/portfolioLogo.png ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\"src\":\"/_next/static/media/portfolioLogo.e8f22fb9.png\",\"height\":730,\"width\":882,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FportfolioLogo.e8f22fb9.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":7});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3B1YmxpYy9wb3J0Zm9saW9Mb2dvLnBuZyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsQ0FBQyw4TUFBOE0iLCJzb3VyY2VzIjpbIkM6XFxVc2Vyc1xcYWJkb3VcXFBvcnRmb2xpb1xccHVibGljXFxwb3J0Zm9saW9Mb2dvLnBuZyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvcG9ydGZvbGlvTG9nby5lOGYyMmZiOS5wbmdcIixcImhlaWdodFwiOjczMCxcIndpZHRoXCI6ODgyLFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRnBvcnRmb2xpb0xvZ28uZThmMjJmYjkucG5nJnc9OCZxPTcwXCIsXCJibHVyV2lkdGhcIjo4LFwiYmx1ckhlaWdodFwiOjd9OyJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./public/portfolioLogo.png\n"));
+
+/***/ })
+
+});
